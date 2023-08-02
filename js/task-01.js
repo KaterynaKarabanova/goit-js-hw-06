86a3d2d7b804c01aa3387b269477df6efabb846c
Kateryna <ikatya7575@gmaiil.com>
2023-08-02 23:01:40 +0300

    test1

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,11 @@
+const categories = document.querySelectorAll(`.item`)
+console.log(categories);
 
+console.log(`Number of categories: ${categories.length}`);
+const title = document.querySelector(`.item > h2`)
+console.log(`Category: ${title.textContent}`);
+const nestedUl = document.querySelector("#categories .item ul");
+
+const liCount = nestedUl.getElementsByTagName("li").length;
+console.log(`Elements: ${liCount}`);
 

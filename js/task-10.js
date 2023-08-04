8929cca3aa3ddeac68a5dae7490fae906797a4c0
Kateryna <ikatya7575@gmaiil.com>
2023-08-04 13:23:07 +0300

    ten tasks done

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,35 @@
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const divBox = document.querySelector("#boxes")
+const createEL = document.querySelector('button[data-create]');
+const destroyEl = document.querySelector('button[data-destroy]');
+const inputField = document.querySelector("input")
+
+
+function createBoxes() {
+  let amount = Number(inputField.value);
+  const boxes = []
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement(`div`)
+    const size = 30 + i * 10
+    box.style.width = `${size}px`
+    box.style.height = `${size}px`
+    box.style.backgroundColor = `${getRandomHexColor()}`
+    boxes.push(box)
+  }
+  divBox.append(...boxes)
+}
+function destroyBoxes() {
+    divBox.innerHTML = '';
+}
+
+createEL.addEventListener("click", createBoxes)
+destroyEl.addEventListener("click", destroyBoxes)
+
